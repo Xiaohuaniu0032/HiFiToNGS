9ff5b8dc407a3bc0fe192fceca442df7e984740b
longfei.fu <lffu_0032@outlook.com>
2023-03-09 09:20:42 +0800

    update

--- test/cmd.sh
+++ test/cmd.sh
@@ -1,1 +1,1 @@
-perl ../HiFiToNGS.pl -fq /root/tools/git_repo/pbAmpliconAnalysis_HLA/data/demultiplex.12878-HG001.fastq -n HG001 -od $PWD
+perl ../HiFiToNGS.pl -fq /root/tools/git_repo/pbAmpliconAnalysis_HLA/data/demultiplex.12878-HG001.fastq -n HG001 -od $PWD -ilen 270 -rlen 150
